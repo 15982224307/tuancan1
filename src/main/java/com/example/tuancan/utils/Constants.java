6aeece6f785e05fea3b3525a3c8ce1809160c85b
xiao <xiao@163.com>
2018-07-22 13:52:29 +0800

    增加微信调试接口，推送事件分析

---
+++ src/main/java/com/example/tuancan/utils/Constants.java
@@ -0,0 +1,30 @@
+package com.example.tuancan.utils;
+
+public interface Constants {
+
+    /**
+     * 公共常量
+     */
+
+    public static class Public{
+
+    }
+    /**
+     * 微信所用常量
+     */
+    public  static class WhChat {
+
+        public static final String EVENT_SUBSCRIBE = "subscribe";
+        public static final String EVENT_UNSUBSCRIBE = "unsubscribe";
+        public static final String EVENT_SCAN = "SCAN";
+        public static final String EVENT_CLICK = "CLICK";
+        public static final String EVENT_VIEW = "VIEW";
+        public static final String EVENT_LOCATION = "LOCATION";
+
+    }
+
+    /**
+     *
+     */
+
+}
