3a0915ffd8cddb37df4e7385ecad6610524296d6
wc950825 <15982224307>
2019-02-14 14:24:25 +0800

    只剩合同

--- src/main/java/com/example/tuancan/TuancanApplication.java
+++ src/main/java/com/example/tuancan/TuancanApplication.java
@@ -8,7 +8,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(value = "com.example.tuancan.dao")
 public class TuancanApplication {
 
-	/*哈哈哈哈哈哈*/
+//	ji
 	public static void main(String[] args) {
 		SpringApplication.run(TuancanApplication.class, args);
 	}
